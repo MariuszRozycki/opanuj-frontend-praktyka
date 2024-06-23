244c3e2260f279d77417e05b4c4e5cad4c8271df
MariuszRozycki <mariansc83@icloud.com>
2024-06-23 15:34:59 +0200

    fix: split SearchForm into smaller components
    - Extracted NameField into a separate component
    - Extracted GenderField into a separate component
    - Extracted SortField into a separate component

---
+++ examples/module1/lesson2/container-components/components/NameField.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type NameFieldProps = {
+  name: string;
+  setName: (name: string) => void;
+}
+
+function NameField({name, setName}: NameFieldProps) {
+  return (
+    <label className="flex flex-col">
+    Name
+    <input
+      className="border h-7 mt-1 indent-2"
+      type="text"
+      placeholder="Rick Sanchez..."
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+    />
+  </label>
+  )
+}
+
+export default NameField;
