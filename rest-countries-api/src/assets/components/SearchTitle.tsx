71ac6115e16dc853f79855fdb1beede465c5d9a5
MariuszRozycki <mariansc83@icloud.com>
2024-07-02 12:27:48 +0200

    Remove rest-countries-api from repo

--- rest-countries-api/src/assets/components/SearchTitle.tsx
+++
@@ -1,7 +0,0 @@
-function SearchTitle({name = "Countries"}: {name?: string}) {
-  return (
-    <h1 className="text-2xl">Search {name}</h1>
-  )
-}
-
-export default SearchTitle;
--- rest-countries-api/src/assets/types/Country.tsx
+++
@@ -1,5 +0,0 @@
-export type Country = {
-  name: { common: string };
-  flags: { png: string };
-  population: number;
-};
