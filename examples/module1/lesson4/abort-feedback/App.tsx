e2306be93c3e591488ba421e66ea6b1908cdc679
MariuszRozycki <mariansc83@icloud.com>
2024-07-12 16:11:52 +0200

    Execution of task instructions: examples/module1/lesson4/abort-feedback

--- examples/module1/lesson4/abort-feedback/App.tsx
+++ examples/module1/lesson4/abort-feedback/App.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
 
 interface User {
   id: number;
@@ -9,29 +10,53 @@ const API_URL = '/api/data/users?timeout=10000';
 
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [isError, setIsError] = useState<boolean>(false);
+  const ulRef = useRef<HTMLUListElement>(null);
+
+  const fetchData = () => {
+    if (ulRef.current) {
+      ulRef.current.innerHTML = '';
+    }
+
+    axios
+      .get(API_URL, { timeout: 5000 })
+      .then((response) => {
+        console.log(response);
+        setUsers(response.data.users);
+        setIsError(false);
+      })
+      .catch((error) => {
+        if (error.code === 'ECONNABORTED') {
+          setIsError(true);
+        }
+      });
+  };
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then(({ users }) => {
-        setUsers(users);
-      });
+    fetchData();
   }, []);
 
   return (
     <div>
       <div className="flex flex-row items-center justify-between py-4">
         <h1 className="text-2xl font-bold">Users</h1>
-        <div className="flex flex-row items-center">
+        {/* Class hidden add to div when error occur */}
+        <div
+          className={`flex flex-row items-center ${isError ? '' : 'hidden'}`}
+        >
           <p className="mr-2">
             Sorry, there seems to be connectivity issues...
           </p>
-          <button className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4">
+          <button
+            className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4"
+            onClick={fetchData}
+          >
             Try again
           </button>
         </div>
       </div>
-      <ul className="space-y-2">
+      {/* Add ulRef and set innerHTML to "" if param timeout is longer then ?timeout=10000 */}
+      <ul ref={ulRef} className="space-y-2">
         {users.map((user, index) => (
           <li
             className="bg-white p-4 rounded-md border border-gray-100"
