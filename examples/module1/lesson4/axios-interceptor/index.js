761ac2b18f0841caaee6416f33796c02944ffa44
MariuszRozycki <mariansc83@icloud.com>
2024-07-13 12:52:09 +0200

    Solving the task examples/module1/lesson4/axios-interceptor

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -1,18 +1,43 @@
 import axios from 'axios';
 
-// Add a request interceptor
-axios.interceptors.request.use(function (config) {
+function addStartTime(config) {
+  config.metadata = { startTime: new Date() };
+  console.log('Request config:', config);
   return config;
-});
+}
+
+function logRequestDuration(config) {
+  const startTime = config.metadata.startTime;
+  const endTime = new Date();
+  const duration = endTime - startTime;
+  console.log(`Request to ${config.url} took ${duration} ms`);
+}
+
+// Add a request interceptor
+axios.interceptors.request.use(addStartTime);
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  // Do something with response data
-  return response;
-});
+axios.interceptors.response.use(
+  function (response) {
+    logRequestDuration(response.config);
+    console.log('response: ', response);
+    return response;
+  },
+  function (error) {
+    if (error.config) {
+      logRequestDuration(error.config);
+    }
+    return Promise.reject(error);
+  }
+);
 
-const {
-  data: { articles },
-} = await axios.get('/api/data/articles?timeout=3000');
+(async () => {
+  try {
+    const response = await axios.get('/api/data/articles?timeout=3000');
+    const articles = response.data.articles;
 
-document.querySelector('#data').innerHTML = articles[0].content;
+    document.querySelector('#data').innerHTML = articles[0].content;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+})();
