f607f8ad08cd47f067e461ca43acd11ead8f8c14
MariuszRozycki <mariansc83@icloud.com>
2024-07-24 13:17:31 +0200

    Add test('should not return any error with valid data')

--- examples/module2/lesson1/discount-validator/validator.test.ts
+++ examples/module2/lesson1/discount-validator/validator.test.ts
@@ -2,18 +2,23 @@ import { describe, expect, test } from 'vitest';
 import { formValidator } from './validator';
 
 describe('Form validation', () => {
-    test('should return an error if first name is missing', () => {
-        const errors = formValidator('', 'Doe', 30);
-        expect(errors).toContain('First name is required');
-    });
+  test('should return an error if first name is missing', () => {
+    const errors = formValidator('', 'Doe', 30);
+    expect(errors).toContain('First name is required');
+  });
 
-    test('should return an error if last name is missing', () => {
-        const errors = formValidator('John', '', 30);
-        expect(errors).toContain('Last name is required');
-    });
+  test('should return an error if last name is missing', () => {
+    const errors = formValidator('John', '', 30);
+    expect(errors).toContain('Last name is required');
+  });
 
-    test('should return an error if age is negative', () => {
-        const errors = formValidator('John', 'Doe', -1);
-        expect(errors).toContain('Age must be a positive number');
-    });
+  test('should return an error if age is negative', () => {
+    const errors = formValidator('John', 'Doe', -1);
+    expect(errors).toContain('Age must be a positive number');
+  });
+
+  test('should not return any error with valid data', () => {
+    const errors = formValidator('John', 'Doe', 30);
+    expect(errors).toHaveLength(0);
+  });
 });
